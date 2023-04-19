6dd701fda16efd41924ffe248d2300083e65f168
ggrivaux <guilhem.grivaux@ensae.fr>
2023-04-19 22:12:32 +0000

    20 Avril 2023

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -1,50 +1,135 @@
 class Graph:
+    """
+    Classe représentant des graphes au moyen de listes d'adjacence
+    et implémentant différents algorithmes sur ceux-ci. Les graphes de
+    cette classe ne sont pas orientés.
+
+    Attributs:
+    -----------
+    nodes: NodeType
+        La liste de noeuds du graphe. Ceux-ci peuvent être de type int, 
+        float ou str mais nous privilégieront les entiers
+    graph: dict
+        Un dictionnaire contenant la liste d'adjacences de chaque noeuds 
+        sous la forme graph[noeud] = [(voisin1, p1, d1), (voisin2, p2, d2), ...]
+        où p1 est la puissance requise sur l'arrete (noeud, voisin1) et d1 
+        la distance
+    nb_nodes: int
+        Le nombre de noeuds du graphe
+    nb_edges: int
+        Le nombre d'aretes du graphe 
+    """
 
     def __init__(self, nodes=[]):
+        """
+        Initialise le graphe avec un ensemble de noeuds (et aucune arête).
+        Parametres: 
+        -----------
+        nodes: liste, optionelle
+            Par défaut elle sera vide.
+        """
         self.nodes = nodes
         self.graph = dict([(n, []) for n in nodes])
         self.nb_nodes = len(nodes)
         self.nb_edges = 0
     
-
     def __str__(self):
+        """Affiche le graphe sous la forme d'une liste de voisins pour 
+        chaque noeud (un par ligne)."""
         if not self.graph:
-            output = "The graph is empty"            
+            output = "Le graphe est vide"            
         else:
-            output = f"The graph has {self.nb_nodes} nodes and {self.nb_edges} edges.\n"
+            output = f"Le graph possède {self.nb_nodes} noeuds et {self.nb_edges} aretes.\n"
             for source, destination in self.graph.items():
                 output += f"{source}-->{destination}\n"
         return output
     
     def add_edge(self, node1, node2, power_min, dist=1):
-       
-       self.nb_edges +=1
-       self.graph[node1].append((node2, power_min, dist))
-       self.graph[node2].append((node1, power_min, dist))
-       raise NotImplementedError
-    
+        """
+        Ajoute une arête au graphe. Les graphes n'étant pas orientés, 
+        une arete est ajoutée à la liste d'adjacence des deux nœuds d'extremite.
+        Parameters: 
+        -----------
+        node1: NodeType
+            Premiere extremite de l'arete
+        node2: NodeType
+            Seconde extremite de l'arete
+        power_min: numerique (int ou float)
+            Puissance minimale sur cette arete
+        dist: numerique (int ou float), optionel
+            Distance entre le nœud 1 et le nœud 2 sur l'arête. La valeur 
+            par defaut est 1.
+        """
+        if node1 not in self.graph:
+            self.graph[node1] = []
+            self.nb_nodes += 1
+            self.nodes.append(node1)
+        if node2 not in self.graph:
+            self.graph[node2] = []
+            self.nb_nodes += 1
+            self.nodes.append(node2)
 
+        self.graph[node1].append((node2, power_min, dist))
+        self.graph[node2].append((node1, power_min, dist))
+        self.nb_edges += 1
+    
     def get_path_with_power(self, src, dest, power):
-        raise NotImplementedError
+        nodes_visited = {node: False for node in self.nodes}
+
+        def dfs(node):
+            if node == dest:
+                return [node]
+            for nei, p, _ in self.graph[node]:
+                if not nodes_visited[nei] and p <= power:
+                    nodes_visited[nei] = True
+                    d = dfs(nei)
+                    if d is not None:
+                        return [node] + d
+            return None
+        return dfs(src)
     
-
     def connected_components(self):
-        raise NotImplementedError
 
+        list_comp = []
+        nodes_visited = {node: False for node in self.nodes}
+
+        def dfs(node):
+            comp = [node]
+            for nei in self.graph[node]:
+                nei = nei[0]
+                if not nodes_visited[nei]:
+                    nodes_visited[nei] = True
+                    comp += dfs(nei)
+            return comp
+        for node in self.nodes:
+            if not nodes_visited[node]:
+                list_comp.append(dfs(node))
+        return list_comp
 
     def connected_components_set(self):
         """
-        The result should be a set of frozensets (one per component), 
-        For instance, for network01.in: {frozenset({1, 2, 3}), frozenset({4, 5, 6, 7})}
+        Le résultat devrait être un ensemble de frozensets (un par composant), 
+        Par exemple pour network01.in: {frozenset({1, 2, 3}), frozenset({4, 5, 6, 7})}
         """
-        return set(map(frozenset, self.connected_components()))
-    
-    def min_power(self, src, dest):
-        """
-        Should return path, min_power. 
-        """
-        raise NotImplementedError
 
+        return set(map(frozenset, self.connected_components()))
+
+    def min_power(self, src, dest):
+        liste = []
+        for n in self.nodes:
+            for i in self.graph[n]:
+                liste.append(i[1])
+        M = max(liste)
+        a = self.get_path_with_power(src, dest, M)
+        if a is None:
+            return None
+        while a is not None:
+            M -= 1
+            a = self.get_path_with_power(src, dest, M)
+        p_min = M+1
+        path = self.get_path_with_power(src, dest, p_min)
+
+        return p_min, path
 
 def graph_from_file(filename):
     """
